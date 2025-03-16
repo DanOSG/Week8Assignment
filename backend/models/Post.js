3a0f673477740d1b641a8b492c3236b39de10581
DanOSG <d.osgood@hotmail.co.uk>
2025-03-16 14:14:05 +0000

    fix for the failed capitalization fix.

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/config');
-const User = require('./user');
+const User = require('./User');
 
 // Define models
 const Post = sequelize.define('Post', {
